100629210d5f893c0db8257571fc7870bf0ec59c
Yerim lee <103623292+lyrWinterCat@users.noreply.github.com>
2022-10-09 21:44:58 +0900

    coding practice

---
+++ kakao/src/level_0/NextNum.java
@@ -0,0 +1,15 @@
+package level_0;
+
+public class NextNum {
+    class Solution {
+        public int solution(int[] common) {
+            int answer = 0;
+            if((common[1]-common[0])==(common[2]-common[1])){
+                answer+=common[common.length-1]+(common[1]-common[0]);
+            } else{
+                answer+=common[common.length-1]*(common[1]/common[0]);
+            }
+            return answer;
+        }
+    }
+}
