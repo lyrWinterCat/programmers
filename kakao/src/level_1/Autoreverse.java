33fef5f3736ee291a4832abefa9bce9d7e8337df
Yerim lee <103623292+lyrWinterCat@users.noreply.github.com>
2022-10-11 19:08:09 +0900

    coding test practice

---
+++ kakao/src/level_1/Autoreverse.java
@@ -0,0 +1,18 @@
+package level_1;
+
+public class Autoreverse {
+    public int[] solution(long n) {
+        String temp = ""+n;
+        int[] answer = new int[temp.length()];
+        for (int i = temp.length()-1; i >=0; i--) {
+            answer[4-i]=Integer.parseInt(""+temp.charAt(i));
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Autoreverse autoreverse = new Autoreverse();
+        long n = 12345;
+        autoreverse.solution(n);
+    }
+}
