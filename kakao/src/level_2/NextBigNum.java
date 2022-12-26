66418faf462bd22a716282138c986082e523747b
Yerim lee <103623292+lyrWinterCat@users.noreply.github.com>
2022-12-26 17:12:18 +0900

    feat : programmers level2 다음 큰 숫자

---
+++ kakao/src/level_2/NextBigNum.java
@@ -0,0 +1,42 @@
+package level_2;
+
+import static java.lang.Integer.toBinaryString;
+
+public class NextBigNum {
+    public int solution(int n) {
+        int answer = 0;
+        String binaryN = toBinaryString(n);
+
+        int cntOne=countOne(binaryN);
+
+        for (int i = n+1; i <=1_000_000 ; i++) {
+            String newBinary = toBinaryString(i);
+            int newBinarycntOne = countOne(newBinary);
+            if(newBinarycntOne==cntOne){
+                answer=i;
+                break;
+            }
+        }
+        return answer;
+    }
+    
+    private int countOne(String binaryN) {
+        int cnt=0;
+        char[] charArr = binaryN.toCharArray();
+        for (char c : charArr) {
+            if(c=='1'){
+                cnt++;
+            }
+        }
+        return cnt;
+        //        return (int) binaryN.chars().filter(c->c=='1').count();
+    }
+
+
+    public static void main(String[] args) {
+        NextBigNum nextBigNum = new NextBigNum();
+        int n = 78;
+        int solution = nextBigNum.solution(n);
+        System.out.println("solution = " + solution);
+    }
+}
