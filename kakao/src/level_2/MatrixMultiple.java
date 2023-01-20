c02a5469507b681c7956542eee5784fdc565dffe
Yerim lee <103623292+lyrWinterCat@users.noreply.github.com>
2023-01-20 19:29:54 +0900

    fix : programmers level2 행렬의 곱 완료

--- kakao/src/level_2/MatrixMultiple.java
+++ kakao/src/level_2/MatrixMultiple.java
@@ -5,18 +5,19 @@ public class MatrixMultiple {
         int[][] answer = new int[arr1.length][arr2[0].length];
 
         for (int i = 0; i < arr1.length; i++) {
-            for (int j = 0; j <arr2[0].length ; j++) {
-                System.out.println("i,j = "+ i +" "+ j);
+            for (int j = 0; j < arr2[0].length; j++) {
+                for (int k = 0; k < arr1[0].length; k++) {
+                    answer[i][j] += arr1[i][k] * arr2[k][j];
+                }
             }
         }
-
         return answer;
     }
 
     public static void main(String[] args) {
         MatrixMultiple matrixMultiple = new MatrixMultiple();
-        int[][] arr1 = {{1,4},{3,2},{4,1}};
-        int[][] arr2 = {{3,3},{3,3}};
+        int[][] arr1 = {{1, 4}, {3, 2}, {4, 1}};
+        int[][] arr2 = {{3, 3}, {3, 3}};
         int[][] solution = matrixMultiple.solution(arr1, arr2);
         for (int[] ints : solution) {
             for (int anInt : ints) {
