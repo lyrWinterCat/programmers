8bc57347e3262c184ba1700a1998022f5c8b1455
Yerim lee <103623292+lyrWinterCat@users.noreply.github.com>
2023-07-31 22:31:16 +0900

    Update SinglyLinkedListTest.java

--- kakao/src/makeList/SinglyLinkedListTest.java
+++ kakao/src/makeList/SinglyLinkedListTest.java
@@ -1,45 +1,48 @@
 package makeList;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class SinglyLinkedListTest {
+    SinglyLinkedList singlyLinkedList;
+
+    @BeforeEach
+    public void makeList(){
+        singlyLinkedList = new SinglyLinkedList();
+    }
 
     @Test
     public void addTest1() throws Exception{
+
         //given
-        SinglyLinkedList test = new SinglyLinkedList();
 
         //when
-        test.add("0");
+        singlyLinkedList.add("0");
 
         //then
-        Assertions.assertEquals("0",test.getFirstNodeData());
-        Assertions.assertEquals(1,test.getSize());
+        Assertions.assertEquals("0",singlyLinkedList.getFirstNodeData());
+        Assertions.assertEquals(1,singlyLinkedList.getSize());
 
     }
 
     @Test
     public void addTest2() throws Exception{
+
         //given
-        SinglyLinkedList test = new SinglyLinkedList();
 
         //when
-        test.add("0");
-        test.add("1");
+        singlyLinkedList.add("0");
+        singlyLinkedList.add("1");
 
         //then
-        Assertions.assertEquals(2,test.getSize());
-        Assertions.assertEquals("0",test.getFirstNodeData());
-        Assertions.assertEquals("1",test.getNodeData(1));
-
+        Assertions.assertEquals(2,singlyLinkedList.getSize());
+        Assertions.assertEquals("0",singlyLinkedList.getFirstNodeData());
+        Assertions.assertEquals("1",singlyLinkedList.getNodeData(1));
     }
 
 
-
-    @org.junit.jupiter.api.Test
+    @Test
     void getSize() {
 
         //given
@@ -54,23 +57,63 @@ class SinglyLinkedListTest {
 
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void getFirstNodeData() {
+        //given
+        SinglyLinkedList list = new SinglyLinkedList();
+
+        //when
+
+
+
+        //then
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void printAllNodesData() {
+        //given
+        SinglyLinkedList list = new SinglyLinkedList();
+
+        //when
+
+
+
+        //then
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void getNodeData() {
+        //given
+        SinglyLinkedList list = new SinglyLinkedList();
+
+        //when
+
+
+
+        //then
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void insert() {
+        //given
+        SinglyLinkedList list = new SinglyLinkedList();
+
+        //when
+
+
+
+        //then
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void delete() {
+        //given
+        SinglyLinkedList list = new SinglyLinkedList();
+
+        //when
+
+
+
+        //then
     }
 }
