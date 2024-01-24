9d8d374b2154c2ce08478edbbb39ab199ec00a9d
lyrWinterCat <dldulfla27@gmail.com>
2024-01-24 18:21:15 +0900

    commit : 무작위 수 뽑기 - 예시는 통과하나 제출시 오답..

---
+++ codingTest/src/level_0/RandomKNumber.java
@@ -0,0 +1,40 @@
+package level_0;
+
+import java.util.*;
+
+public class RandomKNumber {
+    public int[] solution(int[] arr, int k) {
+        Stack<Integer> arr_stack = new Stack<>();
+        arr_stack.push(arr[0]);
+        for (int i = 1; i < arr.length; i++) {
+            if(arr_stack.peek()==arr[i]){
+                continue;
+            }else{
+                arr_stack.push(arr[i]);
+            }
+        }
+
+        System.out.println(arr_stack);
+
+        int[] answer = new int[k];
+        for (int i = 0; i < k; i++) {
+            if(i<arr_stack.size()){
+                answer[i]=arr_stack.get(i);
+            }else{
+                answer[i]=-1;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        RandomKNumber randomKNumber = new RandomKNumber();
+        int[] arr = {0, 1, 1, 2, 2, 3};
+//        int[] arr = {0, 1, 1, 1,1};
+//        int[] arr = {1, 2, 2, 0,0};
+        int k = 4;
+        int[] answer = randomKNumber.solution(arr,k);
+        System.out.println(Arrays.toString(answer));
+    }
+}
