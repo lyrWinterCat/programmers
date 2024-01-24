4371a9acd0a907c64d12d845fbacf571c2052b33
lyrWinterCat <dldulfla27@gmail.com>
2024-01-24 18:24:33 +0900

    commit : 0,1,0,1 등 peek이 아닌 contains로 봐야함 :: 성공

--- codingTest/src/level_0/RandomKNumber.java
+++ codingTest/src/level_0/RandomKNumber.java
@@ -7,7 +7,7 @@ public class RandomKNumber {
         Stack<Integer> arr_stack = new Stack<>();
         arr_stack.push(arr[0]);
         for (int i = 1; i < arr.length; i++) {
-            if(arr_stack.peek()==arr[i]){
+            if(arr_stack.contains(arr[i])){
                 continue;
             }else{
                 arr_stack.push(arr[i]);
@@ -30,10 +30,10 @@ public class RandomKNumber {
 
     public static void main(String[] args) {
         RandomKNumber randomKNumber = new RandomKNumber();
-        int[] arr = {0, 1, 1, 2, 2, 3};
+        int[] arr = {0,1,0,1};
 //        int[] arr = {0, 1, 1, 1,1};
 //        int[] arr = {1, 2, 2, 0,0};
-        int k = 4;
+        int k = 6;
         int[] answer = randomKNumber.solution(arr,k);
         System.out.println(Arrays.toString(answer));
     }
